f3747e7a6019eb0855204ae026ccc43205165a7c
kjam0 <south.kay@gmail.com>
2021-06-19 13:05:34 +1000

    added lists route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get 'lists', to: 'lists#all'
+get 'lists/:id', to: 'lists#show'
+
 end
