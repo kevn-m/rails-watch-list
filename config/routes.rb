245ce34823da84ae8d2b7ee0213f21ff38a4e538
kjam0 <south.kay@gmail.com>
2021-06-19 14:34:40 +1000

    added bookmark controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-get 'lists', to: 'lists#all'
-get 'lists/:id', to: 'lists#show'
+# get 'lists/new', to: 'lists#new'
+# post '/lists', to: 'lists#create'
+# get 'lists', to: 'lists#index'
+# get 'lists/:id', to: 'lists#show', as: :restaurant
 
+resources :lists, only: [:new, :create, :index, :show]
 end
